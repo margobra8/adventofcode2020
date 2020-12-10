2675fd92f57402db06e48a7ed6d7cada328d4c17
Marcos Gómez <margobra8@gmail.com>
2020-12-10 11:58:28 +0100

    add day10

---
+++ day10/day10.py
@@ -0,0 +1,16 @@
+from math import comb, prod
+import re
+
+ratings = sorted(map(int, open('input.txt', 'r').readlines()))
+ratings = [0] + ratings + [ratings[-1] + 3]
+
+print(prod(map(sum, zip(
+	*((b - a == 1, b - a == 3) for a, b in zip(ratings, ratings[1:]))
+))))
+
+# This solution relies on the fact that there are no 2 jolt differences.
+ratings = sorted(map(int, open('input.txt').readlines()))
+ratings = [0] + ratings + [ratings[-1] + 3]
+differences = (b - a for a, b in zip(ratings, ratings[1:]))
+group_lengths = map(len, re.findall(r'(1{2,})', ''.join(map(str, differences))))
+print(prod([1 + comb(length - 1, 1) + comb(length - 1, 2) for length in group_lengths]))
